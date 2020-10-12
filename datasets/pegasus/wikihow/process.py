51234abb7400a17110f2ca3c299b6a5a784554dc
Stas Bekman <stas@stason.org>
2020-10-12 16:16:20 -0700

    almost matching the tfds dataset

--- datasets/pegasus/wikihow/process.py
+++ datasets/pegasus/wikihow/process.py
@@ -6,18 +6,28 @@ import json
 import re
 
 def get_art_abs_wikihow(path):
-    articles = glob.glob('%s/*' % path)
+    articles = sorted(glob.glob('%s/*' % path))
     for a in articles:
         try:
             with open(a, 'r') as f:
                 text = f.read()
             splits = text.split('@article')
             
-            abstract = splits[0].replace('@summary', '')
-            abstract = " ".join(l.strip().capitalize() for l in abstract.split("\n"))
+            abstract = splits[0].replace('@summary\n', '')
+            #abstract = re.sub(r'\\', r'\\\\', abstract)
+            abstract = "<n>".join(l.strip().capitalize() for l in abstract.split("\n"))
+            abstract = re.sub(r'(<n>)+', '<n>', abstract)
+            abstract = re.sub(r'<n>$', '', abstract)
             abstract = re.sub(' +', ' ', abstract).strip()
-            article = splits[1].replace('\n', ' ').replace('@article', '').strip()
             
+            #article = splits[1].replace('\n', ' ').replace('@article', '').strip()
+            article = splits[1].replace('@article\n', '').strip()
+            article = re.sub(r'\n$', '', article)
+            #article = re.sub(r'\\', r'\\\\', article)
+            article = re.sub(r'\n', '<n>', article)
+            #article  = article.replace("\n", "<n>")
+            article = re.sub(' +', ' ', article).strip()
+                
             yield article, abstract
         except Exception as e:
             yield None
@@ -33,22 +43,30 @@ def write_to_bin(lines, out_prefix):
             # Get the strings to write to .bin file
             if line == None: continue
             article, abstract = line
+            article_len, abstract_len = len(article), len(abstract)
+            
 
             #print(f"article {article}")
             #print(f"abstract {abstract}")
             #  a threshold is used to remove short articles with long summaries as well as articles with no summary
-            if len(abstract) < (0.75*len(article)):
+            if article_len and abstract_len and abstract_len < 0.75*article_len:
                 # remove extra commas in abstracts
-                abstract = abstract.replace(".,",".")
+                #abstract = abstract.replace(".,",".")
                 # remove extra commas in articles
-                article = article.replace(";,", "")
-                article = article.replace(".,",".")
-                article = re.sub(r'[.]+[\n]+[,]',".\n", article)
+                #article = article.replace(";,", "")
+                #article = article.replace(".,",".")
+                #article = re.sub(r'[.]+[\n]+[,]',".\n", article)
+                
+                # truncate final \n
+                #article = re.sub(r'\n$', '', article)
 
                 #abstract = abstract.strip().replace("\n", "")
-                article  = article.replace("\n", " ")
-                article = re.sub(' +', ' ', article).strip()
-
+                #article  = article.replace("\n", "<n>")
+                #article = re.sub(' +', ' ', article).strip()
+                
+                article  = re.sub(r'[\r\n]', '<n>', article)
+                abstract = re.sub(r'[\r\n]', '<n>', abstract)
+                
                 # Write article and abstract to files
                 source_file.write(article + '\n')
                 target_file.write(abstract + '\n')
@@ -61,10 +79,10 @@ def create_stories(save_path='data'):
     lines = get_art_abs_wikihow('./test_articles')
     write_to_bin(lines, os.path.join(save_path, "test"))
 
-    lines = get_art_abs_wikihow('./valid_articles')
-    write_to_bin(lines, os.path.join(save_path, "val"))
+    # lines = get_art_abs_wikihow('./valid_articles')
+    # write_to_bin(lines, os.path.join(save_path, "val"))
 
-    lines = get_art_abs_wikihow('./train_articles')
-    write_to_bin(lines, os.path.join(save_path, "train"))
+    # lines = get_art_abs_wikihow('./train_articles')
+    # write_to_bin(lines, os.path.join(save_path, "train"))
 
 create_stories()
